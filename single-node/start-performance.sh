1c40d88310234d4f0ede5685a55799799a5e0911
Sachin-Mamoru <sachinm@wso2.com>
2023-05-30 15:43:10 +0530

    concurrency updated

--- single-node/start-performance.sh
+++ single-node/start-performance.sh
@@ -47,6 +47,7 @@ mode=""
 jwt_token_client_secret=""
 jwt_token_user_password=""
 enable_burst=false
+concurrency=""
 
 results_dir="$PWD/results-$timestamp"
 default_minimum_stack_creation_wait_time=10
@@ -75,7 +76,7 @@ function usage() {
     echo ""
 }
 
-while getopts "k:c:j:n:u:p:i:b:w:y:g:t:m:h" opts; do
+while getopts "k:c:j:n:u:p:i:b:w:r:y:g:t:m:h" opts; do
     case $opts in
     k)
         key_file=${OPTARG}
@@ -104,6 +105,9 @@ while getopts "k:c:j:n:u:p:i:b:w:y:g:t:m:h" opts; do
     w)
         minimum_stack_creation_wait_time=${OPTARG}
         ;;
+    r)
+        concurrency=${OPTARG}
+        ;;
     y)
         jwt_token_client_secret=${OPTARG}
         ;;
@@ -130,7 +134,7 @@ shift "$((OPTIND - 1))"
 
 echo "Run mode: $mode"
 run_performance_tests_options="$@"
-run_performance_tests_options+=(" -v $mode -k $jwt_token_client_secret -o $jwt_token_user_password -b $enable_burst")
+run_performance_tests_options+=(" -r $concurrency -v $mode -k $jwt_token_client_secret -o $jwt_token_user_password -b $enable_burst")
 
 if [[ ! -f $key_file ]]; then
     echo "Please provide the key file."
--- two-node-cluster/start-performance.sh
+++ two-node-cluster/start-performance.sh
@@ -49,6 +49,7 @@ mode=""
 jwt_token_client_secret=""
 jwt_token_user_password=""
 enable_burst=false
+concurrency=""
 
 results_dir="$PWD/results-$timestamp"
 default_minimum_stack_creation_wait_time=10
@@ -79,7 +80,7 @@ function usage() {
     echo ""
 }
 
-while getopts "k:c:j:n:u:p:d:e:i:b:w:y:g:t:m:h" opts; do
+while getopts "k:c:j:n:u:p:d:e:i:b:w:r:y:g:t:m:h" opts; do
     case $opts in
     k)
         key_file=${OPTARG}
@@ -114,6 +115,9 @@ while getopts "k:c:j:n:u:p:d:e:i:b:w:y:g:t:m:h" opts; do
     w)
         minimum_stack_creation_wait_time=${OPTARG}
         ;;
+    r)
+        concurrency=${OPTARG}
+        ;;
     y)
         jwt_token_client_secret=${OPTARG}
         ;;
@@ -140,7 +144,7 @@ shift "$((OPTIND - 1))"
 
 echo "Run mode: $mode"
 run_performance_tests_options="$@"
-run_performance_tests_options+=(" -v $mode -k $jwt_token_client_secret -o $jwt_token_user_password -b $enable_burst")
+run_performance_tests_options+=(" -r $concurrency -v $mode -k $jwt_token_client_secret -o $jwt_token_user_password -b $enable_burst")
 
 if [[ ! -f $key_file ]]; then
     echo "Please provide the key file."
