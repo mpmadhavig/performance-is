86e437ab22b6dcd52e9f888c715ce4e6dd3c5d43
Sachin-Mamoru <sachinm@wso2.com>
2023-09-03 21:00:16 +0530

    with user attributes 500

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -242,24 +242,24 @@
 #    [skip]=false
 #    [modes]="FULL QUICK PUBLISH"
 #)
-#declare -A test_scenario25=(
-#    [name]="07-oidc_password_grant_user_attributes"
-#    [display_name]="OIDC Password Grant Type User Attributes"
-#    [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
-#    [jmx]="oidc/OIDC_Password_Grant_User_Attributes.jmx"
-#    [tenantMode]=false
-#    [skip]=false
-#    [modes]="FULL QUICK PUBLISH"
-#)
-declare -A test_scenario26=(
-    [name]="05-oidc_auth_code_redirect_with_consent_group"
-    [display_name]="OIDC Auth Code Grant Redirect With Consent Group"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-    [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent_Group.jmx"
+declare -A test_scenario25=(
+    [name]="07-oidc_password_grant_user_attributes"
+    [display_name]="OIDC Password Grant Type User Attributes"
+    [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
+    [jmx]="oidc/OIDC_Password_Grant_User_Attributes.jmx"
     [tenantMode]=false
     [skip]=false
     [modes]="FULL QUICK PUBLISH"
 )
+#declare -A test_scenario26=(
+#    [name]="05-oidc_auth_code_redirect_with_consent_group"
+#    [display_name]="OIDC Auth Code Grant Redirect With Consent Group"
+#    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
+#    [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent_Group.jmx"
+#    [tenantMode]=false
+#    [skip]=false
+#    [modes]="FULL QUICK PUBLISH"
+#)
 #declare -A test_scenario27=(
 #    [name]="05-oidc_auth_code_redirect_with_consent_roles"
 #    [display_name]="OIDC Auth Code Grant Redirect With Consent Roles"
