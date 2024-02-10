c4eda22780028efbe1dba532dad933e5d77ba643
Madhavi Gayathri <47152272+mpmadhavig@users.noreply.github.com>
2024-02-10 12:08:10 +0530

    Enable cc saml sso oidc only.

--- two-node-cluster/run-performance-tests.sh
+++ two-node-cluster/run-performance-tests.sh
@@ -67,15 +67,15 @@ db_password="wso2carbon"
 #     [skip]=false
 #     [modes]="FULL"
 # )
-# declare -A test_scenario4=(
-#     [name]="04-oauth_client_credential_grant"
-#     [display_name]="Client Credentials Grant Type"
-#     [description]="Obtain an access token using the OAuth 2.0 client credential grant type."
-#     [jmx]="oauth/OAuth_Client_Credentials_Grant.jmx"
-#     [tenantMode]=false
-#     [skip]=false
-#     [modes]="FULL"
-# )
+declare -A test_scenario4=(
+    [name]="04-oauth_client_credential_grant"
+    [display_name]="Client Credentials Grant Type"
+    [description]="Obtain an access token using the OAuth 2.0 client credential grant type."
+    [jmx]="oauth/OAuth_Client_Credentials_Grant.jmx"
+    [tenantMode]=false
+    [skip]=false
+    [modes]="FULL"
+)
 declare -A test_scenario5=(
     [name]="05-oidc_auth_code_redirect_with_consent"
     [display_name]="OIDC Auth Code Grant Redirect With Consent"
@@ -112,15 +112,15 @@ declare -A test_scenario5=(
 #     [skip]=false
 #     [modes]="FULL"
 # )
-# declare -A test_scenario9=(
-#     [name]="09-saml2_sso_redirect_binding"
-#     [display_name]="SAML2 SSO Redirect Binding"
-#     [description]="Obtain a SAML 2 assertion response using redirect binding."
-#     [jmx]="saml/SAML2_SSO_Redirect_Binding.jmx"
-#     [tenantMode]=false
-#     [skip]=false
-#     [modes]="FULL QUICK"
-# )
+declare -A test_scenario9=(
+    [name]="09-saml2_sso_redirect_binding"
+    [display_name]="SAML2 SSO Redirect Binding"
+    [description]="Obtain a SAML 2 assertion response using redirect binding."
+    [jmx]="saml/SAML2_SSO_Redirect_Binding.jmx"
+    [tenantMode]=false
+    [skip]=false
+    [modes]="FULL QUICK"
+)
 # declare -A test_scenario10=(
 #    [name]="10-oauth_auth_code_redirect_with_consent_tenant"
 #    [display_name]="Auth Code Grant Redirect With Consent"
