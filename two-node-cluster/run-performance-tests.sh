c6cee930baaac5e68f54bf92c2edbc157e81d9c9
Madhavi Gayathri <47152272+mpmadhavig@users.noreply.github.com>
2024-02-12 17:19:28 +0530

    Enable tests.

--- two-node-cluster/run-performance-tests.sh
+++ two-node-cluster/run-performance-tests.sh
@@ -31,15 +31,15 @@ db_password="wso2carbon"
 # Execute common script
 . $script_dir/perf-test-is.sh
 
-# declare -A test_scenario0=(
-#     [name]="00-authenticate_super_tenant_users"
-#     [display_name]="Authenticate Super Tenant User"
-#     [description]="Select random super tenant users and authenticate through the RemoteUserStoreManagerService."
-#     [jmx]="authenticate/Authenticate_Super_Tenant_User.jmx"
-#     [tenantMode]=false
-#     [skip]=false
-#     [modes]="FULL QUICK"
-# )
+declare -A test_scenario0=(
+    [name]="00-authenticate_super_tenant_users"
+    [display_name]="Authenticate Super Tenant User"
+    [description]="Select random super tenant users and authenticate through the RemoteUserStoreManagerService."
+    [jmx]="authenticate/Authenticate_Super_Tenant_User.jmx"
+    [tenantMode]=false
+    [skip]=false
+    [modes]="FULL QUICK"
+)
 # declare -A test_scenario1=(
 #     [name]="01-oauth_auth_code_redirect_with_consent"
 #     [display_name]="Auth Code Grant Redirect With Consent"
@@ -67,24 +67,24 @@ db_password="wso2carbon"
 #     [skip]=false
 #     [modes]="FULL"
 # )
-# declare -A test_scenario4=(
-#     [name]="04-oauth_client_credential_grant"
-#     [display_name]="Client Credentials Grant Type"
-#     [description]="Obtain an access token using the OAuth 2.0 client credential grant type."
-#     [jmx]="oauth/OAuth_Client_Credentials_Grant.jmx"
-#     [tenantMode]=false
-#     [skip]=false
-#     [modes]="FULL"
-# )
-# declare -A test_scenario5=(
-#     [name]="05-oidc_auth_code_redirect_with_consent"
-#     [display_name]="OIDC Auth Code Grant Redirect With Consent"
-#     [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-#     [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent.jmx"
-#     [tenantMode]=false
-#     [skip]=false
-#     [modes]="FULL QUICK"
-# )
+declare -A test_scenario4=(
+    [name]="04-oauth_client_credential_grant"
+    [display_name]="Client Credentials Grant Type"
+    [description]="Obtain an access token using the OAuth 2.0 client credential grant type."
+    [jmx]="oauth/OAuth_Client_Credentials_Grant.jmx"
+    [tenantMode]=false
+    [skip]=false
+    [modes]="FULL"
+)
+declare -A test_scenario5=(
+    [name]="05-oidc_auth_code_redirect_with_consent"
+    [display_name]="OIDC Auth Code Grant Redirect With Consent"
+    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
+    [jmx]="oidc/OIDC_AuthCode_Redirect_WithConsent.jmx"
+    [tenantMode]=false
+    [skip]=false
+    [modes]="FULL QUICK"
+)
 # declare -A test_scenario6=(
 #     [name]="06-oidc_implicit_redirect_with_consent"
 #     [display_name]="OIDC Implicit Grant Redirect With Consent"
