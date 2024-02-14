6802e95c851bbc9f5f1b92d35602e858f850be2c
Madhavi Gayathri <47152272+mpmadhavig@users.noreply.github.com>
2024-02-14 11:58:01 +0530

    enable only implicit.

--- two-node-cluster/run-performance-tests.sh
+++ two-node-cluster/run-performance-tests.sh
@@ -58,15 +58,15 @@ db_password="wso2carbon"
 #     [skip]=false
 #     [modes]="FULL"
 # )
-declare -A test_scenario3=(
-    [name]="03-oauth_password_grant"
-    [display_name]="Password Grant Type"
-    [description]="Obtain an access token using the OAuth 2.0 password grant type."
-    [jmx]="oauth/OAuth_Password_Grant.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL"
-)
+# declare -A test_scenario3=(
+#     [name]="03-oauth_password_grant"
+#     [display_name]="Password Grant Type"
+#     [description]="Obtain an access token using the OAuth 2.0 password grant type."
+#     [jmx]="oauth/OAuth_Password_Grant.jmx"
+#     [tenantMode]=false
+#     [skip]=false
+#     [modes]="FULL"
+# )
 # declare -A test_scenario4=(
 #     [name]="04-oauth_client_credential_grant"
 #     [display_name]="Client Credentials Grant Type"
@@ -85,24 +85,24 @@ declare -A test_scenario3=(
 #     [skip]=false
 #     [modes]="FULL QUICK"
 # )
-# declare -A test_scenario6=(
-#     [name]="06-oidc_implicit_redirect_with_consent"
-#     [display_name]="OIDC Implicit Grant Redirect With Consent"
-#     [description]="Obtain an access token and an id token using the OAuth 2.0 implicit grant type."
-#     [jmx]="oidc/OIDC_Implicit_Redirect_WithConsent.jmx"
-#     [tenantMode]=false
-#     [skip]=false
-#     [modes]="FULL"
-# )
-declare -A test_scenario7=(
-    [name]="07-oidc_password_grant"
-    [display_name]="OIDC Password Grant Type"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
-    [jmx]="oidc/OIDC_Password_Grant.jmx"
+declare -A test_scenario6=(
+    [name]="06-oidc_implicit_redirect_with_consent"
+    [display_name]="OIDC Implicit Grant Redirect With Consent"
+    [description]="Obtain an access token and an id token using the OAuth 2.0 implicit grant type."
+    [jmx]="oidc/OIDC_Implicit_Redirect_WithConsent.jmx"
     [tenantMode]=false
     [skip]=false
-    [modes]="FULL QUICK"
+    [modes]="FULL"
 )
+# declare -A test_scenario7=(
+#     [name]="07-oidc_password_grant"
+#     [display_name]="OIDC Password Grant Type"
+#     [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
+#     [jmx]="oidc/OIDC_Password_Grant.jmx"
+#     [tenantMode]=false
+#     [skip]=false
+#     [modes]="FULL QUICK"
+# )
 # declare -A test_scenario8=(
 #     [name]="08-oidc_request_path_authenticator"
 #     [display_name]="OIDC Auth Code Request Path Authenticator With Consent"
