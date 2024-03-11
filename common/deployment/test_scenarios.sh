13426ef58f12645345b13d702acf17e714a89792
Madhavi Gayathri <madhavi.17@cse.mrt.ac.lk>
2024-03-11 13:51:21 +0530

    Enable oidc password test case.

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -89,24 +89,24 @@
 #     [skip]=false
 #     [modes]="FULL QUICK PUBLISH"
 # )
-declare -A test_scenario08=(
-    [name]="08-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_groups_and_roles"
-    [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes Groups and Roles"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-    [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK PUBLISH OIDC_AUTH_CODE_REDIRECT_WITHOUT_CONSENT_UA_GROUPS_ROLES_FLOW"
-)
-# declare -A test_scenario09=(
-#     [name]="09-oidc_password_grant"
-#     [display_name]="OIDC Password Grant Type"
-#     [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
-#     [jmx]="oidc/OIDC_Password_Grant.jmx"
+# declare -A test_scenario08=(
+#     [name]="08-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_groups_and_roles"
+#     [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes Groups and Roles"
+#     [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
+#     [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
 #     [tenantMode]=false
 #     [skip]=false
-#     [modes]="FULL QUICK PUBLISH"
+#     [modes]="FULL QUICK PUBLISH OIDC_AUTH_CODE_REDIRECT_WITHOUT_CONSENT_UA_GROUPS_ROLES_FLOW"
 # )
+declare -A test_scenario09=(
+    [name]="09-oidc_password_grant"
+    [display_name]="OIDC Password Grant Type"
+    [description]="Obtain an access token and an id token using the OAuth 2.0 password grant type."
+    [jmx]="oidc/OIDC_Password_Grant.jmx"
+    [tenantMode]=false
+    [skip]=false
+    [modes]="FULL QUICK PUBLISH"
+)
 # declare -A test_scenario10=(
 #     [name]="10-oidc_password_grant_retrieve_user_attributes"
 #     [display_name]="OIDC Password Grant Type Retrieve User Attributes"
