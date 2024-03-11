c87c65e96b8ec3a48cb6812ae3710b855b0e0f42
Madhavi Gayathri <madhavi.17@cse.mrt.ac.lk>
2024-03-11 13:29:37 +0530

    Enable the correct test case.

--- common/deployment/test_scenarios.sh
+++ common/deployment/test_scenarios.sh
@@ -80,24 +80,24 @@
 #     [skip]=false
 #     [modes]="FULL QUICK PUBLISH"
 # )
-declare -A test_scenario07=(
-    [name]="07-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_and_groups"
-    [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes and Groups"
-    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-    [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_And_Groups.jmx"
-    [tenantMode]=false
-    [skip]=false
-    [modes]="FULL QUICK PUBLISH"
-)
-# declare -A test_scenario08=(
-#     [name]="08-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_groups_and_roles"
-#     [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes Groups and Roles"
+# declare -A test_scenario07=(
+#     [name]="07-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_and_groups"
+#     [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes and Groups"
 #     [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
-#     [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
+#     [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_And_Groups.jmx"
 #     [tenantMode]=false
 #     [skip]=false
-#     [modes]="FULL QUICK PUBLISH OIDC_AUTH_CODE_REDIRECT_WITHOUT_CONSENT_UA_GROUPS_ROLES_FLOW"
+#     [modes]="FULL QUICK PUBLISH"
 # )
+declare -A test_scenario08=(
+    [name]="08-oidc_auth_code_redirect_without_consent_retrieve_user_attributes_groups_and_roles"
+    [display_name]="OIDC Auth Code Grant Redirect Without Consent Retrieve User Attributes Groups and Roles"
+    [description]="Obtain an access token and an id token using the OAuth 2.0 authorization code grant type."
+    [jmx]="oidc/OIDC_AuthCode_Redirect_WithoutConsent_Retrieve_User_Attributes_Groups_And_Roles.jmx"
+    [tenantMode]=false
+    [skip]=false
+    [modes]="FULL QUICK PUBLISH OIDC_AUTH_CODE_REDIRECT_WITHOUT_CONSENT_UA_GROUPS_ROLES_FLOW"
+)
 # declare -A test_scenario09=(
 #     [name]="09-oidc_password_grant"
 #     [display_name]="OIDC Password Grant Type"
